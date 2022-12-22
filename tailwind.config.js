6c407ac6048d3d9e6649ddebe2bf1e5fd6932f41
Mokshit06 <50412128+Mokshit06@users.noreply.github.com>
2022-12-22 13:38:07 +0530

    chore: improve tailwind setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}', './twind/cache.txt'],
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './.tw-variants'],
   theme: {
     extend: {},
   },
